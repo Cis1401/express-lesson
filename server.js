ace62074ca49df394cba718b7e8885f8b42d390e
Jiezel Olandag <agum.jhay@gmail.com>
2023-05-12 15:00:03 +0800

    save

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require('express');
 
 // routes
-const userRouter = require('./routes/users.router');
-const productsRouter = require('./routes/products.router');
+// const userRouter = require('./routes/users.router');
+// const productsRouter = require('./routes/products.router');
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 // Register a middleware function that parses incoming JSON payloads/request
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
 
-app.post('/greeting', (req, res) => {
-    const name = req.body.name;
-    const greeting = `Hello Master ${name}`;
-    res.send(greeting);
-});
+// app.post('/greeting', (req, res) => {
+//     const name = req.body.name;
+//     const greeting = `Hello Master ${name}`;
+//     res.send(greeting);
+// });
 
-app.use('/users', userRouter);
+// app.use('/users', userRouter);
 
 // http://localhost:3000/change-password (HTTP PUT METHOD)
 
